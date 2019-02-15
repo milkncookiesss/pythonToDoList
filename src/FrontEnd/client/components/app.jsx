4d0dc53c773c5707e9e2db3018d27fb164bd1c74
milkncookiesss <p.parkcontact@gmail.com>
2019-02-14 23:27:50 -0800

    "testing react front end and webpack"

---
+++ src/FrontEnd/client/components/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+}
+
+render() {
+  return (
+    <div>
+      hello this is a web page from react
+    </div>
+  )
+}
+
+export default App;
